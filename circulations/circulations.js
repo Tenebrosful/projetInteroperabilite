2aeb4b4d12c725a1848e86e66ce3b6cc1776b675
Tenebrosful <hugobernard39@gmail.com>
2022-01-13 14:56:49 +0100

    Import base

---
+++ circulations/circulations.js
@@ -0,0 +1,23 @@
+/**
+ * @var axios
+ */
+
+// @ts-ignore
+const mymap = L.map('mapid').setView([47.3803867, -1.7113826], 17);
+
+// @ts-ignore
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    {
+        maxZoom: 20,
+        attribution: 'Map data: <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>' +
+            '<a href="https://icons8.com/icon/113531/musée">Musée icon by Icons8</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+
+const mairieIcon = L.icon({iconUrl: "https://img.icons8.com/bubbles/50/000000/museum.png", iconSize: [40,40]});
+
+L.marker([47.3803867, -1.7113826], {title: "Mairie de Notre-Dame-des-Landes", alt: "Position sur la carte de la Mairie de Notre-Dame-des-Landes", icon: mairieIcon}).addTo(mymap)
+    .bindPopup("Mairie de Notre-Dame-des-Landes");
---
+++ circulations/circulations.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Mapping Vélo</title>
+    <meta charset="utf-8"/>
+
+    <style>
+        body {
+            margin: 0;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossorigin=""/>
+
+</head>
+<body>
+<div id="mapid" style="width: 100%; height: 100vh;"></div>
+
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script src="circulations.js"></script>
+</body>
+</html>
+
