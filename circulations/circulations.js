c07c7ee0350289b742469e70eb0923af6e5060f3
Tenebrosful <hugobernard39@gmail.com>
2022-01-13 15:40:21 +0100

    Add incidents markers

--- circulations/circulations.js
+++ circulations/circulations.js
@@ -21,3 +21,25 @@ const mairieIcon = L.icon({iconUrl: "https://img.icons8.com/bubbles/50/000000/mu
 
 L.marker([47.3803867, -1.7113826], {title: "Mairie de Notre-Dame-des-Landes", alt: "Position sur la carte de la Mairie de Notre-Dame-des-Landes", icon: mairieIcon}).addTo(mymap)
     .bindPopup("Mairie de Notre-Dame-des-Landes");
+
+axios.get("https://data.loire-atlantique.fr/api/records/1.0/search/?dataset=224400028_info-route-departementale&q=&facet=nature&facet=type&facet=datepublication")
+.then((res) => {
+    res.data.records.forEach(incident => {
+        console.log("New incident", incident)
+        const title = getIncidentTitle(incident);
+        L.marker([incident.geometry.coordinates[1], incident.geometry.coordinates[0]], {title: title, alt: "Position sur la carte de l'incident: " + title }).addTo(mymap)
+            .bindPopup(getIncidentPopup(incident));
+    });
+});
+
+function getIncidentTitle(incident) {
+    return `${incident.fields.ligne1} (${incident.fields.ligne2} ${incident.fields.ligne4})`;
+}
+
+function getIncidentPopup(incident) {
+    return `<center>${incident.fields.nature}<br/>
+    ${incident.fields.ligne1} (${incident.fields.type})<br/>
+    ${incident.fields.ligne2}<br/>
+    ${incident.fields.ligne4}<br/>
+    </center>`
+}
